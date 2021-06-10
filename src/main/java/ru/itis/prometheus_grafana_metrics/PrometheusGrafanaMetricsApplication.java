e70277db2c04514ab376b4f11d713670e5f12e29
Ruslan Ruslan <ruselkomp1@gmail.com>
2021-06-10 21:34:26 +0300

    Init commit

---
+++ src/main/java/ru/itis/prometheus_grafana_metrics/PrometheusGrafanaMetricsApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.prometheus_grafana_metrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometheusGrafanaMetricsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrometheusGrafanaMetricsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itis/prometheus_grafana_metrics/PrometheusGrafanaMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.prometheus_grafana_metrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusGrafanaMetricsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
